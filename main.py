147b6d6c27e12c8bf26eb7426cc9a75b3a75d75c
fallnomega <fallnomega@gmail.com>
2023-01-17 09:02:27 -0800

    game dev

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ word_list = ['able', 'about', 'account', 'acid', 'across', 'act', 'addition', 'a
 'boat', 'body', 'boiling', 'bone', 'book', 'boot', 'bottle', 'box', 'boy', 'brain', 'brake', 'branch', 'brass', 'bread']
 
 chosen_word = random.choice(word_list)
-#Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
 display = []
 show_display=''
@@ -26,24 +23,26 @@ for x in chosen_word:
     display+='_'
 
 #Testing code
-print(f'Pssst, the solution is: {chosen_word}.')
 
-#Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-print('Guess a letter: ')
-#guess = input("Guess a letter: ").lower()
-guess = 'e'
-
-#Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-#Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
+Use a while loop to let the user guess again.
+# The loop should only stop once the user has guessed all the letters in
+# the chosen_word and 'display' has no more blanks ("_").
+# Then you can tell the user they've won.
+you_win = False
 word_length = len(chosen_word)
-for x in range(word_length):
-    letter = chosen_word[x]
-    if guess == letter:
-        display[x]=letter
+result=""
 
+while you_win==False:
+    print(f'Pssst, the solution is: {chosen_word}.')
+    guess = input("Guess a letter: ").lower()
+    result = ""
+    for x in range(word_length):
+        letter = chosen_word[x]
+        if guess == letter:
+            display[x]=letter
+            for x in display:
+                result += x
 
-#Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-print(display)
+    if result == chosen_word:
+        you_win=True
+        print (f"You WIN! The word was {chosen_word}")
